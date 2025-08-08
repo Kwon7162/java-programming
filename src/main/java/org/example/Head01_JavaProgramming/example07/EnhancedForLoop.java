cebbf5df4fa8f1b1d3990fa821d1ba86756b95c7
권찬호 <rnjscksdud17@gmail.com>
2025-08-08 15:49:41 +0900

    Array

---
+++ src/main/java/org/example/Head01_JavaProgramming/example07/EnhancedForLoop.java
@@ -0,0 +1,10 @@
+package org.example.Head01_JavaProgramming.example07;
+
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        String[] animals = {"Cat", "Dog", "Bird", "Cow", "Horse", "Shark", "Elephant", "Fox", };
+        for (String animal : animals) {
+            System.out.println(animal);
+        }
+    }
+}
