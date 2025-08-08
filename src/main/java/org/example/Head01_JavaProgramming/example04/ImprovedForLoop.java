93f9dc1e82da939803021837fc7f274c054705f9
권찬호 <rnjscksdud17@gmail.com>
2025-08-08 14:52:24 +0900

    Variable and if&switch

---
+++ src/main/java/org/example/Head01_JavaProgramming/example04/ImprovedForLoop.java
@@ -0,0 +1,12 @@
+package org.example.Head01_JavaProgramming.example04;
+
+public class ImprovedForLoop {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        for (int n: numbers) {
+            System.out.println("numbers 출력: " + n);
+
+        }
+    }
+}
