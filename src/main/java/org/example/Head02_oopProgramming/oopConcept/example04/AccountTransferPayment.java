918bf817e5c016a9796f627b1442746ce9497ab5
권찬호 <rnjscksdud17@gmail.com>
2025-08-12 11:54:21 +0900

    Add Crypto & extended abstract class

--- src/main/java/org/example/Head02_oopProgramming/oopConcept/example04/AccountTransferPayment.java
+++ src/main/java/org/example/Head02_oopProgramming/oopConcept/example04/AccountTransferPayment.java
@@ -1,6 +1,6 @@
 package org.example.Head02_oopProgramming.oopConcept.example04;
 
-public class AccountTransferPayment implements Payment{
+public class AccountTransferPayment extends AbstractPayment{
     @Override
     public void pay(double amount) {
         System.out.println("계좌이체로 " + amount + "원 결제 완료.");
--- src/main/java/org/example/Head02_oopProgramming/oopConcept/example04/CreditCardPayment.java
+++ src/main/java/org/example/Head02_oopProgramming/oopConcept/example04/CreditCardPayment.java
@@ -1,6 +1,6 @@
 package org.example.Head02_oopProgramming.oopConcept.example04;
 
-public class CreditCardPayment implements Payment {
+public class CreditCardPayment extends AbstractPayment {
     @Override
     public void pay(double amount) {
         System.out.println("신용카드로 " + amount + "원 결제 완료.");
