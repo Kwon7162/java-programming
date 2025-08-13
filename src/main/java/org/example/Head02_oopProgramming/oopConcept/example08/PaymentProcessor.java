d21555f75fb3f2486fd32de6caa1efad8d177426
권찬호 <rnjscksdud17@gmail.com>
2025-08-13 10:42:50 +0900

    SOLID-OCP Example

---
+++ src/main/java/org/example/Head02_oopProgramming/oopConcept/example08/PaymentProcessor.java
@@ -0,0 +1,7 @@
+package org.example.Head02_oopProgramming.oopConcept.example08;
+
+public class PaymentProcessor {
+    public void pay(Payment payment, double amount) {
+        payment.pay(amount);
+    }
+}
