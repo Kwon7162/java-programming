cde697938b5fb2938a77ab090a525e81cd4b6fb0
권찬호 <rnjscksdud17@gmail.com>
2025-08-18 10:50:10 +0900

    ArrayListExample

--- src/main/java/org/example/Head03_JCF/example01/ArrayVsArrayList.java
+++ src/main/java/org/example/Head03_JCF/example01/ArrayVsArrayList.java
@@ -8,16 +8,19 @@ public class ArrayVsArrayList {
     public static void main(String[] args) {
         // Array 예시
         int[] arr = new int[3];
-        arr[0] = 10; arr[1] = 20; arr[2] = 30;
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 30;
         System.out.println("Array: " + Arrays.toString(arr));
-        // arr[3] = 40; // 런타임 ArrayIndexOutOfBoundsException
 
-        // ArrayList 예시
+        //ArrayList Ex
         List<Integer> list = new ArrayList<>();
-        list.add(10); list.add(20); list.add(30);
+        list.add(10);
+        list.add(20);
+        list.add(30);
         list.add(40);
         System.out.println("ArrayList: " + list);
         list.remove(Integer.valueOf(20));
-        System.out.println("removed 20 → " + list);
+        System.out.println("removed 20 ->: " + list);
     }
 }
