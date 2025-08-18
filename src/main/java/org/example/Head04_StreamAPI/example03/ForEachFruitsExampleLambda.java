53349bdaa8d71983b5405fe0424e2bf6499ae8c0
권찬호 <rnjscksdud17@gmail.com>
2025-08-18 14:55:21 +0900

    ForEachFruitsExample

---
+++ src/main/java/org/example/Head04_StreamAPI/example03/ForEachFruitsExampleLambda.java
@@ -0,0 +1,13 @@
+package org.example.Head04_StreamAPI.example03;
+
+import java.util.List;
+
+public class ForEachFruitsExampleLambda {
+    public static void main(String[] args) {
+        List<String> fruits = List.of("Apple", "Banana", "Cherry");
+
+        fruits.stream()
+                .forEach(fruit -> System.out.println("Fruit: " + fruit));
+
+    }
+}
