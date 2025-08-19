3aded8b49a0bfdfffc856f2e8e472c1e28e5500f
권찬호 <rnjscksdud17@gmail.com>
2025-08-19 10:30:48 +0900

    Stream API 고급연산

---
+++ src/main/java/org/example/Head04_StreamAPI/example07/SummarizingExample.java
@@ -0,0 +1,45 @@
+package org.example.Head04_StreamAPI.example07;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+public class SummarizingExample {
+    public static void main(String[] args) {
+        List<Integer> intScores = Arrays.asList(80, 95, 70, 100, 85);
+        List<Double> doubleScores = Arrays.asList(82.3, 62.0, 72.3, 64.4, 93.2);
+        List<Long> longScores = Arrays.asList(80L, 95L, 70L, 100L, 85L);
+
+        // TODO IntSummaryStatistics 구현
+        IntSummaryStatistics intSummaryStatistics = intScores.stream()
+                        .collect(Collectors.summarizingInt(Integer::intValue));
+
+        // TODO DoubleSummaryStatistics 구현
+        DoubleSummaryStatistics doubleSummaryStatistics = doubleScores.stream()
+                .collect(Collectors.summarizingDouble(Double::doubleValue));
+
+        // TODO LongSummaryStatistics 구현
+        LongSummaryStatistics longSummaryStatistics = longScores.stream()
+                        .collect(Collectors.summarizingLong(Long::longValue));
+
+        System.out.println("Int Summary Count = " + intSummaryStatistics.getCount());
+        System.out.println("Int Summary Sum = " + intSummaryStatistics.getSum());
+        System.out.println("Int Summary Min = " + intSummaryStatistics.getMin());
+        System.out.println("Int Summary Max = " + intSummaryStatistics.getMax());
+        System.out.println("Int Summary Average = " + intSummaryStatistics.getAverage());
+        System.out.println();
+
+        System.out.println("Double Summary Count = " + doubleSummaryStatistics.getCount());
+        System.out.println("Double Summary Sum = " + doubleSummaryStatistics.getSum());
+        System.out.println("Double Summary Min = " + doubleSummaryStatistics.getMin());
+        System.out.println("Double Summary Max = " + doubleSummaryStatistics.getMax());
+        System.out.println("Double Summary Average = " + doubleSummaryStatistics.getAverage());
+        System.out.println();
+
+        System.out.println("Long Summary Count = " + longSummaryStatistics.getCount());
+        System.out.println("Long Summary Sum = " + longSummaryStatistics.getSum());
+        System.out.println("Long Summary Min = " + longSummaryStatistics.getMin());
+        System.out.println("Long Summary Max = " + longSummaryStatistics.getMax());
+        System.out.println("Long Summary Average = " + longSummaryStatistics.getAverage());
+    }
+}
